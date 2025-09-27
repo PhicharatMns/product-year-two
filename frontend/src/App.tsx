4323bdb79ad309dfab1e892e3a0f2309dc529bec
YouBeer-gif <PhicharatMns@gmail.com>
2025-09-27 15:37:05 +0700

    flex login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Sidebar from "./component/sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./component/Sidebar";
 import Login from "./component/Login";
 // import AddEmployee from "./AddEmployee";
 // import EmployeeList from "./EmployeeList";
@@ -8,12 +8,20 @@ import Login from "./component/Login";
 export default function App() {
   return (
     <BrowserRouter>
-      <Sidebar />
-      <Routes>
-        {/* <Route path="/" element={<AddEmployee />} />
-        <Route path="/list" element={<EmployeeList />} /> */}
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      {/* flex แบ่งสองฝั่ง */}
+      <div className="flex min-h-screen">
+        {/* Sidebar ฝั่งซ้าย */}
+        <Sidebar />
+
+        {/* Content ฝั่งขวา */}
+        <div className="flex-1 ">
+          <Routes>
+            {/* <Route path="/" element={<AddEmployee />} /> */}
+            {/* <Route path="/list" element={<EmployeeList />} /> */}
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
