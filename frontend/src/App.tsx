65f5252e8c10a69ea3e522c594c0cb5e02b39f54
YouBeer-gif <PhicharatMns@gmail.com>
2025-09-27 15:37:42 +0700

    data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./component/Sidebar";
 import Login from "./component/Login";
+import Sidebar from "./component/sidebar";
 // import AddEmployee from "./AddEmployee";
 // import EmployeeList from "./EmployeeList";
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* flex แบ่งสองฝั่ง */}
       <div className="flex min-h-screen">
-        {/* Sidebar ฝั่งซ้าย */}
         <Sidebar />
 
-        {/* Content ฝั่งขวา */}
         <div className="flex-1 ">
           <Routes>
             {/* <Route path="/" element={<AddEmployee />} /> */}
