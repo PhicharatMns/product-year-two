dae1d894e95a91a1a1e521f9f4f97e68ef8d9a07
YouBeer-gif <PhicharatMns@gmail.com>
2025-09-29 14:40:01 +0700

    data

--- frontend/src/Admin/Home.tsx
+++ frontend/src/Admin/Home.tsx
@@ -7,12 +7,13 @@ export default function Home() {
             <div className="flex-1">
                 <div className='container mx-auto  h-200 my-5'>
                     <div className="grid grid-cols-2 gap-5 ">
-                        <div className="border h-50 relative">
+                        <div className="border h-100 relative">
                             <img
                                 src="https://png.pngtree.com/thumb_back/fh260/background/20250326/pngtree-futuristic-data-dashboard-image_17146904.jpg"
                                 alt=""
                                 className="absolute w-full h-full object-cover object-center rounded-xl "
                             />
+                            
                         </div>
                         <div className="grid grid-cols-2 gap-5">
                             <div className="border relative">
