bb383866630dac28198818f802394aa791a0589b
YouBeer-gif <PhicharatMns@gmail.com>
2025-10-02 21:14:16 +0700

    vs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,8 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Navigate
+  Navigate,
 } from "react-router-dom";
-
 
 import Searchpastjobs from "./Admin/Searchpastjobs";
 import Editacc from "./Admin/Editacc";
@@ -18,23 +17,21 @@ import Profile from "./Admin/profile";
 import Profileadmin from "./Admin/Profileadmin";
 import Popupalert from "./Admin/popupalert";
 
-
 const router = createBrowserRouter([
-
   { path: "/", element: <Navigate to="/logins" replace /> },
-  { path: '/logins', element: <Login /> },
+  { path: "/logins", element: <Login /> },
   {
     element: <AdminLayout />,
     children: [
       { path: "home", element: <Home /> },
       { path: "editacc", element: <Editacc /> },
       { path: "searchpastjobs", element: <Searchpastjobs /> },
-      { path: 'details', element: <Details /> },
-      { path: 'EmployeeList', element: <EmployeeList /> },
-      { path: 'AddEmployee', element: <AddEmployee /> },
-       { path: 'Profile', element: <Profile /> },
-       {path: 'Profileadmin', element: < Profileadmin/>},
-        {path: 'Popupalert', element: < Popupalert/>}
+      { path: "details", element: <Details /> },
+      { path: "EmployeeList", element: <EmployeeList /> },
+      { path: "AddEmployee", element: <AddEmployee /> },
+      { path: "Profile", element: <Profile /> },
+      { path: "Profileadmin", element: <Profileadmin /> },
+      { path: "Popupalert", element: <Popupalert /> },
     ],
   },
 ]);
