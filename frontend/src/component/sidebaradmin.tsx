7505872741f3864895405699c94f17871318f496
YouBeer-gif <PhicharatMns@gmail.com>
2025-10-06 13:21:23 +0700

    add

--- frontend/src/component/sidebaradmin.tsx
+++ frontend/src/component/sidebaradmin.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { AiOutlineCaretRight } from "react-icons/ai";
+
 import { Link } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
 import { IoIosLogOut } from "react-icons/io";
@@ -26,29 +25,27 @@ export default function Sidebaradmin() {
     { text: "ออกจากระบบ", icons: IoIosLogOut, Link: "Logins" },
   ];
 
-  const [onClickSizebar, setOnClickSizebar] = useState(true);
+  // const [onClickSizebar, setOnClickSizebar] = useState(false);
 
   return (
     <div className="fixed z-20 flex h-screen">
-    
+
       <div
-        className={` flex relative  flex-col justify-between bg-blue-500 text-white font-bold border-r transition-all duration-300
-          ${onClickSizebar ? "w-16" : "w-64"}`}
+        className={` flex relative  flex-col justify-between w-64 bg-blue-500 text-white font-bold border-r transition-all duration-300
+        `}
       >
 
         <div className="  flex flex-col ">
-   
+
           <div className="flex items-center py-3 p-4">
             <Link to="/Profile" className="mx-auto">
               <div
-                className={`uppercase text-2xl font-black text-white whitespace-nowrap mt-5 transition-all duration-300 ${
-                  onClickSizebar ? "opacity-0 w-0 overflow-hidden" : "opacity-100 w-auto"
+                className={`uppercase text-2xl font-black text-white whitespace-nowrap mt-5 transition-all duration-300 
                 }`}
               >
                 Tech<span className="text-yellow-500">Job</span>
                 <div
-                  className={`text-xs text-white mt-1 transition-all duration-300 ${
-                    onClickSizebar ? "opacity-0 w-0 overflow-hidden" : "opacity-100 w-auto"
+                  className={`text-xs text-white mt-1 transition-all duration-300
                   }`}
                 >
                   Admin
@@ -56,15 +53,15 @@ export default function Sidebaradmin() {
               </div>
             </Link>
 
-            <button className="relative mt-5 cursor-pointer text-white">
+            {/* <button className="relative mt-5 cursor-pointer text-white">
               <AiOutlineCaretRight
                 onClick={() => setOnClickSizebar(!onClickSizebar)}
                 size={30}
               />
-            </button>
+            </button> */}
           </div>
 
-    
+
           <div className="mt-4 flex flex-col">
             {datasizebar.map((event, index) => {
               const Icons = event.icons;
@@ -73,9 +70,7 @@ export default function Sidebaradmin() {
                   <div className="flex items-center gap-2 my-2 pl-5 py-3 cursor-pointer hover:bg-yellow-500 duration-300">
                     <Icons size={24} />
                     <span
-                      className={`whitespace-nowrap inline-block transition-all duration-300 ${
-                        onClickSizebar ? "w-0 opacity-0 overflow-hidden" : "w-auto opacity-100"
-                      }`}
+                      className={`whitespace-nowrap inline-block transition-all duration-300 `}
                     >
                       {event.text}
                     </span>
@@ -86,33 +81,28 @@ export default function Sidebaradmin() {
           </div>
         </div>
 
-      
+
         <Link to="/Profile" className="mt-auto">
           <div
-            className={`border-blue-900 bg-blue-900 duration-300 hover:bg-blue-700 h-20 flex items-center gap-4 cursor-pointer px-2 ${
-              onClickSizebar ? "justify-center" : "justify-start px-5"
-            }`}
+            className={`border-blue-900 bg-blue-900 duration-300 hover:bg-blue-700 h-20 flex items-center gap-4 cursor-pointer px-2
+              }`}
           >
             <img
               src="https://i.pinimg.com/736x/7e/46/c6/7e46c6d2798eff446b365c5246f4c9ca.jpg"
-              className={`object-cover rounded-full duration-300 ${
-                onClickSizebar ? "w-10 h-10" : "w-14 h-14"
-              }`}
+              className={`object-cover w-10 rounded-full duration-300  `}
               alt="pic"
             />
-            {!onClickSizebar && (
-              <div className="text-lg font-semibold text-white">
-                คุณ จักรยาน สีแดง
-                <div className="text-sm text-yellow-400">
-                  Admin
-                </div>
+            <div className="text-lg font-semibold text-white">
+              คุณ จักรยาน สีแดง
+              <div className="text-sm text-yellow-400">
+                Admin
               </div>
-              
-            )}
+            </div>
+            
           </div>
         </Link>
       </div>
 
-     </div>
+    </div>
   );
 }
