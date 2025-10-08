c5ad09ecd705a94b2037be24064b4ecfbadd2205
Natz <91265149+LIFNAT@users.noreply.github.com>
2025-10-08 09:09:20 +0700

    User SideBar

--- frontend/src/component/sidebar.tsx
+++ frontend/src/component/sidebar.tsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import { AiOutlineCaretRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
-import { BsEnvelopePaper } from "react-icons/bs";
-import { MdOutlineMoreTime } from "react-icons/md";
-import { BsPencilSquare } from "react-icons/bs";
-import { FaTools } from "react-icons/fa";
-import { SlCalender } from "react-icons/sl";
 import { IoIosLogOut } from "react-icons/io";
-
-
+import { TbBellPlus } from "react-icons/tb";
+import { GoGraph } from "react-icons/go";
+import { LiaUserEditSolid } from "react-icons/lia";
+import { VscNewFile } from "react-icons/vsc";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { BsEnvelopePaper } from "react-icons/bs";
+import { FaInbox } from "react-icons/fa6";
 
 
 interface SidebarItem {
@@ -18,68 +17,90 @@ interface SidebarItem {
   Link: string;
 }
 
-export default function Sidebar() {
-  const datasizebar: SidebarItem[] = [
-    { text: "Dashboard", icons: MdDashboard, Link: "Dashboard" },
-    { text: "รับใบงาน", icons: BsEnvelopePaper, Link: "paper" },
-    { text: "บันทึกเวลา", icons: MdOutlineMoreTime, Link: "Time" },
-    { text: "ลงนาม", icons: BsPencilSquare, Link: "Name" },
-        { text: "บันทึกวัสดุ", icons: FaTools, Link: "tool" },
-    { text: "ปฏิทิน", icons: SlCalender , Link: "caleder" },
-    { text: "ออกจากระบบ", icons: IoIosLogOut, Link: "login" },
+export default function SidebarUser() {
+  const [open, setOpen] = useState(false);
 
+  const datasizebar: SidebarItem[] = [
+    { text: "Dashboard", icons: MdDashboard, Link: "DashboardUser" },
+    { text: "รับใบงาน", icons: BsEnvelopePaper, Link: "GetPaper" },
+    { text: "กล่องข้อความ", icons: FaInbox, Link: "Box" },
+    { text: "สถิติ", icons: GoGraph, Link: "graph" },
+    // { text: "กำหนดประเภท/หมวด", icons: MdOutlineCategory, Link: "setwork" },
+    { text: "จัดการบัญชีช่าง", icons: LiaUserEditSolid, Link: "Editacc" },
+    { text: "ออกจากระบบ", icons: IoIosLogOut, Link: "Logins" },
   ];
 
-  const [onClickSizebar, setOnClickSizebar] = useState(true);
-
   return (
-    <div
-      className={`h-screen font-bold border-r transition-all duration-300 bg-blue-500 text-white  ${onClickSizebar ? "w-16" : "w-64"
-        }`}
-    >
-      {/* headerSizebar */}
-      <div className="flex items-center py-3 p-4">
-        <span
-          className={`uppercase mx-auto text-2xl font-black text-white whitespace-nowrap mt-5 ${onClickSizebar ? "opacity-0 w-0" : "opacity-100 w-auto"
-            }`}
+    <div className="relative">
+    
+      <div className="lg:hidden fixed top-4 left-4 z-30">
+        <button
+          onClick={() => setOpen(!open)}
+          className="bg-blue-600 p-2 rounded-lg text-white shadow-lg focus:outline-none"
         >
-          Tech
-          <span className="text-yellow-500">
-            Job
-          </span>
-        </span>
-        <button className=" relative mt-5 text-white" >
-          <AiOutlineCaretRight
-            onClick={() => setOnClickSizebar(!onClickSizebar)}
-            size={30}
-          />
+          {open ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
         </button>
       </div>
-      {/* menuSizeBar */}
-      <div className="mt-4">
-        {datasizebar.map((event, index) => {
-          const Icons = event.icons;
-          return (
-            <div
-              key={index}
-              className="flex items-center py-3 p-4 text-lg gap-5"
-            >
-              {/* Icon */}
-              <div className="min-w-[30px] flex justify-center mt-4">
-                <Icons size={24} />
+
+     
+      {open && (
+        <div
+          className="fixed inset-0 w-0 z-10 lg:hidden"
+          onClick={() => setOpen(false)}
+        ></div>
+      )}
+
+     
+      <div
+        className={`fixed z-20  flex flex-col justify-between h-screen bg-blue-500 text-white font-bold border-r transition-all duration-300
+        ${open ? "translate-x-0" : "-translate-x-full"} 
+        lg:translate-x-0 lg:w-64 w-64`}
+      >
+        {/* Logo */}
+        <div className="flex flex-col">
+          <div className="flex items-center py-3 p-4">
+            <Link to="/Profile" className="mx-auto">
+              <div className="uppercase text-2xl font-black text-white whitespace-nowrap mt-5">
+                Tech<span className="text-yellow-500">Job</span>
+       
               </div>
-              {/* text */}
-              <span
-                className={`whitespace-nowrap mt-4 ${onClickSizebar ? "opacity-0 w-0" : "opacity-100 w-auto"
-                  }`}
-              >
-                <Link to={`/${event.Link}`}>{event.text}</Link>
-              </span>
+            </Link>
+          </div>
+
+          {/* เมนู */}
+          <div className="mt-4 flex flex-col">
+            {datasizebar.map((event, index) => {
+              const Icons = event.icons;
+              return (
+                <Link
+                  to={`/${event.Link}`}
+                  key={index}
+                  onClick={() => setOpen(false)} // ปิดเมนูเมื่อคลิกในมือถือ
+                >
+                  <div className="flex items-center gap-2 my-2 pl-5 py-3 cursor-pointer hover:bg-yellow-500 duration-300">
+                    <Icons size={24} />
+                    <span>{event.text}</span>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+
+        <Link to="/Profile" className="mt-auto">
+          <div className="border-blue-900 bg-blue-900 hover:bg-blue-700 h-20 flex items-center gap-4 cursor-pointer px-2 duration-300">
+            <img
+              src="https://i.pinimg.com/736x/7e/46/c6/7e46c6d2798eff446b365c5246f4c9ca.jpg"
+              className="object-cover w-10 rounded-full"
+              alt="pic"
+            />
+            <div className="text-lg font-semibold text-white">
+              คุณ ลำไย ลายย้อน
+             
             </div>
-          );
-        })}
+          </div>
+        </Link>
       </div>
-      
     </div>
   );
 }
