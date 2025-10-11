d4166bed437e8d90960721b24c6d586336ed3873
YouBeer-gif <PhicharatMns@gmail.com>
2025-10-11 09:05:56 +0700

    เเก้ไข  Path Editacc 100% to Path Details / ID

--- backend/models/Tradesman.js
+++ backend/models/Tradesman.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const tradesmanSchema = new mongoose.Schema({
-  Worksheet: String,
-  Employer: String,
-  Contact_number: String,
-  address: String,
-  responsible: String,
-  Date_of_acceptance_of_work: String,
-  Closing_date: String,
-  description: String,
-  image: String,
-  tradesmen: [{ type: mongoose.Schema.Types.ObjectId, ref: "TradesmanProfile" }] // เพิ่มตรงนี้
+  Name: String,
+  Nickname: String,
+  ID: String,
+  Birthday: String,
+  Address: String,
+  Phone_Number: String,
+  Email: String,
+  Profile: String,
+  Position: String,
+  Start_data: String,
 });
 
 module.exports = mongoose.model("Tradesman", tradesmanSchema);
