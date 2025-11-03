f07cc23f953d0c7777fca77a76073ee507aabda1
YouBeer-gif <PhicharatMns@gmail.com>
2025-11-03 09:23:24 +0700

    commit

--- frontend/src/executive/Getpaperexecutive.tsx
+++ frontend/src/executive/Getpaperexecutive.tsx
@@ -1,7 +1,17 @@
-export default function Getpaperexecutive () {
-    return (
-        <div>
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import the AOS CSS
+import { useEffect } from 'react';
 
+export default function Getpaperexecutive() {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+    return (
+        <div data-aos="fade-zoom-in"
+            data-aos-easing="ease-in-back"
+            data-aos-delay="100"
+            data-aos-offset="1" className="h-screen bg-yellow-500">
+            s
         </div>
     )
 }
