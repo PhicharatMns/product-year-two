cd34a647d2a626d965f67e7cf8d497b887261894
YouBeer-gif <PhicharatMns@gmail.com>
2025-11-03 11:19:10 +0700

    commit

--- frontend/src/Admin/Dashboard.tsx
+++ frontend/src/Admin/Dashboard.tsx
@@ -6,34 +6,45 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  CartesianGrid
 } from "recharts";
 import { useTheme } from "@/components/theme-provider";
 import { animate, motion, useMotionValue, useTransform } from "motion/react";
 import { useEffect } from "react";
 
+
+
 export default function Dashboard() {
-  const materialData = [
-    { name: "งานไม้", quantity: 57 },
-    { name: "งานไฟฟ้า", quantity: 54 },
-    { name: "งานประปา", quantity: 25 },
-    { name: "งานปูน", quantity: 89 },
-    { name: "งานสี", quantity: 45 },
-    { name: "งานกระเบื้อง", quantity: 24 },
-    { name: "งานเชื่อม", quantity: 23 },
+  const techniciansData = [
+    { เดือน: 'มกราคม', จำนวนช่าง: 50, ช่างที่ได้งาน: 35, ช่างที่ยังไม่มีงาน: 15 },
+    { เดือน: 'กุมภาพันธ์', จำนวนช่าง: 60, ช่างที่ได้งาน: 40, ช่างที่ยังไม่มีงาน: 20 },
+    { เดือน: 'มีนาคม', จำนวนช่าง: 55, ช่างที่ได้งาน: 45, ช่างที่ยังไม่มีงาน: 10 },
+    { เดือน: 'เมษายน', จำนวนช่าง: 65, ช่างที่ได้งาน: 50, ช่างที่ยังไม่มีงาน: 15 },
+    { เดือน: 'พฤษภาคม', จำนวนช่าง: 70, ช่างที่ได้งาน: 60, ช่างที่ยังไม่มีงาน: 10 },
+    { เดือน: 'มิถุนายน', จำนวนช่าง: 60, ช่างที่ได้งาน: 40, ช่างที่ยังไม่มีงาน: 20 },
+    { เดือน: 'กรกฎาคม', จำนวนช่าง: 75, ช่างที่ได้งาน: 55, ช่างที่ยังไม่มีงาน: 20 },
+    { เดือน: 'สิงหาคม', จำนวนช่าง: 80, ช่างที่ได้งาน: 65, ช่างที่ยังไม่มีงาน: 15 },
+    { เดือน: 'กันยายน', จำนวนช่าง: 85, ช่างที่ได้งาน: 70, ช่างที่ยังไม่มีงาน: 15 },
+    { เดือน: 'ตุลาคม', จำนวนช่าง: 90, ช่างที่ได้งาน: 75, ช่างที่ยังไม่มีงาน: 15 },
+    { เดือน: 'พฤศจิกายน', จำนวนช่าง: 95, ช่างที่ได้งาน: 80, ช่างที่ยังไม่มีงาน: 15 },
+    { เดือน: 'ธันวาคม', จำนวนช่าง: 100, ช่างที่ได้งาน: 85, ช่างที่ยังไม่มีงาน: 15 },
   ];
 
-  const WorkMountData = [
-    { name: "ม.ค.", quantity: 40 },
-    { name: "ก.พ.", quantity: 65 },
-    { name: "มี.ค.", quantity: 50 },
-    { name: "เม.ย.", quantity: 70 },
-    { name: "พ.ค.", quantity: 90 },
-    { name: "มิ.ย.", quantity: 80 },
-    { name: "ก.ค.", quantity: 110 },
-    { name: "ส.ค.", quantity: 25 },
-    { name: "ต.ค.", quantity: 75 },
-    { name: "พ.ย.", quantity: 25 },
-    { name: "ธ.ค.", quantity: 10 },
+
+  // #region Sample data
+  const monthlyData = [
+    { เดือน: 'มกราคม', ทั้งหมด: 50, เสร็จสิ้น: 20, กำลังดำเนินการ: 30 },
+    { เดือน: 'กุมภาพันธ์', ทั้งหมด: 60, เสร็จสิ้น: 35, กำลังดำเนินการ: 25 },
+    { เดือน: 'มีนาคม', ทั้งหมด: 45, เสร็จสิ้น: 30, กำลังดำเนินการ: 15 },
+    { เดือน: 'เมษายน', ทั้งหมด: 70, เสร็จสิ้น: 50, กำลังดำเนินการ: 20 },
+    { เดือน: 'พฤษภาคม', ทั้งหมด: 55, เสร็จสิ้น: 25, กำลังดำเนินการ: 30 },
+    { เดือน: 'มิถุนายน', ทั้งหมด: 65, เสร็จสิ้น: 40, กำลังดำเนินการ: 25 },
+    { เดือน: 'กรกฎาคม', ทั้งหมด: 60, เสร็จสิ้น: 45, กำลังดำเนินการ: 15 },
+    { เดือน: 'สิงหาคม', ทั้งหมด: 75, เสร็จสิ้น: 50, กำลังดำเนินการ: 25 },
+    { เดือน: 'กันยายน', ทั้งหมด: 80, เสร็จสิ้น: 60, กำลังดำเนินการ: 20 },
+    { เดือน: 'ตุลาคม', ทั้งหมด: 70, เสร็จสิ้น: 45, กำลังดำเนินการ: 25 },
+    { เดือน: 'พฤศจิกายน', ทั้งหมด: 90, เสร็จสิ้น: 70, กำลังดำเนินการ: 20 },
+    { เดือน: 'ธันวาคม', ทั้งหมด: 100, เสร็จสิ้น: 80, กำลังดำเนินการ: 20 },
   ];
 
   // #endregion
@@ -46,30 +57,21 @@ export default function Dashboard() {
   }, []);
 
   const { theme } = useTheme();
-  const text = theme === "dark" ? "text-yellow-500" : "text-gray-800";
   const descrtiption = theme === "dark" ? "text-white" : "text-gray-500";
-  const cardBg = theme === "dark" ? "bg-gray-700" : "bg-blue-50/40";
-  const barColor1 = theme === "dark" ? "#60a5fa" : "#3b82f6";
-  const barColor2 = theme === "dark" ? "#34d399" : "#10b981";
-  const axisColor = theme === "dark" ? "#ddd" : "#555";
-  const tooltipBg = theme === "dark" ? "#1f2937" : "white";
-  const tooltipBorder = theme === "dark" ? "#374151" : "#e5e7eb";
-  const tooltipText = theme === "dark" ? "#f9fafb" : "#111827";
+  const cardBg = theme === "dark" ? "bg-gray-900  " : "";
 
   return (
     <div className="w-max-380 p-6 mx-auto container pt-10">
       <div className="">
         <header className="p-5">
           <h1
-            className={`text-3xl sm:text-3xl md:text-3xl font-extrabold drop-shadow-sm  ${
-              theme === "dark" ? "text-yellow-500" : "text-blue-500"
-            }`}
+            className={`text-3xl sm:text-3xl md:text-3xl font-extrabold drop-shadow-sm  ${theme === "dark" ? "text-yellow-500" : "text-blue-500"
+              }`}
           >
             Dashboard{" "}
             <span
-              className={`${
-                theme === "dark" ? "text-white" : "text-yellow-500"
-              }`}
+              className={`${theme === "dark" ? "text-white" : "text-yellow-500"
+                }`}
             >
               Admin
             </span>
@@ -85,24 +87,31 @@ export default function Dashboard() {
               {[
                 {
                   title: "จำนวนช่าง",
-                  color: "bg-gray-700",
+                  color: "bg-blue-400",
+                  color_bark: 'bg-yellow-500'
+                },
+                {
+                  title: "ช่างที่ได้งาน",
+                  color: "bg-blue-500",
+                  color_bark: 'bg-yellow-600'
+
                 },
                 {
                   title: "ช่างที่ยังไม่มีงาน",
-                  color: "bg-blue-600",
+                  color: "bg-blue-700",
+                  color_bark: 'bg-yellow-700'
+
                 },
                 {
-                  title: "กำลังดำเนิน",
-                  color: "bg-yellow-500",
-                },
-                {
-                  title: "งานที่เสร็จแล้ว",
-                  color: "bg-green-500",
+                  title: "รายการขอเบิกของ",
+                  color: "bg-blue-800",
+                  color_bark: 'bg-yellow-900'
+
                 },
               ].map((item, index) => (
                 <div
                   key={index}
-                  className={`${item.color} text-white rounded-2xl flex  pl-5 justify-center flex-col shadow-lg`}
+                  className={`  text-white rounded-2xl flex  pl-5 justify-center flex-col shadow-lg ${theme === 'dark' ? item.color_bark : item.color}`}
                 >
                   <h2 className="text-2xl mb-3 font-extrabold">{item.title}</h2>
                   <p className="mb-3 font-extrabold text-xl">
@@ -111,9 +120,12 @@ export default function Dashboard() {
                   <div className="text-sm text-white">
                     <button
                       role="link"
-                      className={`relative bg-[linear-gradient(#ffffff,#ffffff),linear-gradient(#EAB308,#EAB308)]  bg-[length:100%_2px,0_2px] bg-[position:100%_100%,0_100%]  bg-no-repeat   transition-[background-size,color] duration-500  hover:bg-[0_2px,100%_2px]   rounded-md ${
-                        theme === "dark" ? "hover:text-yellow-500" : "hover:text-blue-500"
-                      }`}
+                      className={`relative cursor-pointer bg-[length:100%_2px,0_2px] bg-[position:100%_100%,0_100%] 
+                 bg-no-repeat transition-[background-size,color] duration-500 hover:bg-[0_2px,100%_2px] 
+                 rounded-md ${theme === "dark"
+                          ? "hover:text-purple-600 bg-[linear-gradient(#ffffff,#ffffff),linear-gradient(#7c3aed,#7c3aed)]" // ม่วงเข้มตัดเหลือง
+                          : "hover:text-yellow-500 bg-[linear-gradient(#ffffff,#ffffff),linear-gradient(#facc15,#facc15)]" // น้ำเงินเข้มตัดเหลือง
+                        }`}
                     >
                       รายละเอียด
                     </button>
@@ -123,79 +135,54 @@ export default function Dashboard() {
             </div>
 
             <div className="w-full flex flex-col gap-3">
-              <div className={`rounded-3xl shadow-inner p-6 ${cardBg}`}>
+              <div className={`rounded-xl shadow-xl p-6 ${cardBg}`}>
                 <h2
-                  className={`text-2xl font-extrabold mb-4 text-center ${text}`}
+                  className={`text-2xl font-extrabold mb-4 text-center ${theme === 'dark' ? 'text-yellow-500' : 'text-blue-500'}`}
                 >
-                  งานทั้งหมด
+                  จํานวนกราฟที่ได่รับงาน
                 </h2>
-                <div className="w-full h-70">
+                <div className="w-full h-70 ">
                   <ResponsiveContainer width="100%" height="100%">
-                    <BarChart data={materialData} barSize={35}>
-                      <XAxis
-                        dataKey="name"
-                        stroke={axisColor}
-                        tick={{ fontSize: 14, fill: axisColor }}
-                      />
-                      <YAxis
-                        stroke={axisColor}
-                        tick={{ fontSize: 14, fill: axisColor }}
-                      />
-                      <Tooltip
-                        contentStyle={{
-                          backgroundColor: tooltipBg,
-                          borderRadius: "12px",
-                          border: `1px solid ${tooltipBorder}`,
-                          color: tooltipText,
-                        }}
-                      />
-                      <Legend
-                        wrapperStyle={{ fontSize: "14px", color: tooltipText }}
-                      />
-                      <Bar
-                        dataKey="quantity"
-                        fill={barColor1}
-                        radius={[8, 8, 0, 0]}
-                      />
+                    <BarChart
+                      data={techniciansData}>
+                      <CartesianGrid
+                        strokeDasharray="3 3"
+                        stroke={theme === 'dark' ? '#facc15' : '#3b82f6'}
+                      />                      <XAxis dataKey="เดือน" />
+                      <YAxis yAxisId="left" orientation="left" stroke={`${theme === 'dark' ? '#eab308' : '#3b82f6'}`} width="auto" />
+                      <YAxis yAxisId="right" orientation="right" stroke={`${theme === 'dark' ? '#eab308' : '#3b82f6'}`} width="auto" />
+                      <Tooltip />
+                      <Legend />
+                      <Bar yAxisId="left" dataKey="จำนวนช่าง" fill={` ${theme === 'dark' ? '#3b82f6' : '#3b82f6'}`} />
+                      <Bar yAxisId="right" dataKey="ช่างที่ได้งาน" fill={` ${theme === 'dark' ? '#10b981' : '#10b981'}`} />
+                      <Bar yAxisId="right" dataKey="ช่างที่ยังไม่มีงาน" fill={` ${theme === 'dark' ? '#ef4444' : '#ef4444'}`} />
                     </BarChart>
                   </ResponsiveContainer>
                 </div>
               </div>
 
-              <div className={`rounded-3xl shadow-inner p-6 ${cardBg}`}>
+              <div className={`rounded-xl shadow-xl p-6 ${cardBg}`}>
                 <h2
-                  className={`text-2xl font-extrabold mb-4 text-center ${text}`}
+                  className={`text-2xl font-extrabold mb-4 text-center ${theme === 'dark' ? 'text-yellow-500' : 'text-blue-500'}`}
                 >
                   จำนวนงานแต่ละเดือน
                 </h2>
                 <div className="w-full h-70">
                   <ResponsiveContainer width="100%" height="100%">
-                    <BarChart data={WorkMountData} barSize={35}>
-                      <XAxis
-                        dataKey="name"
-                        stroke={axisColor}
-                        tick={{ fontSize: 14, fill: axisColor }}
+                    <BarChart
+                      data={monthlyData}>
+                      <CartesianGrid
+                        strokeDasharray="3 3"
+                        stroke={theme === 'dark' ? '#facc15' : '#3b82f6'}
                       />
-                      <YAxis
-                        stroke={axisColor}
-                        tick={{ fontSize: 14, fill: axisColor }}
-                      />
-                      <Tooltip
-                        contentStyle={{
-                          backgroundColor: tooltipBg,
-                          borderRadius: "12px",
-                          border: `1px solid ${tooltipBorder}`,
-                          color: tooltipText,
-                        }}
-                      />
-                      <Legend
-                        wrapperStyle={{ fontSize: "14px", color: tooltipText }}
-                      />
-                      <Bar
-                        dataKey="quantity"
-                        fill={barColor2}
-                        radius={[8, 8, 0, 0]}
-                      />
+                      <XAxis dataKey="เดือน" />
+                      <YAxis yAxisId="left" orientation="left" stroke={`${theme === 'dark' ? '#eab308' : '#3b82f6'}`} width="auto" />
+                      <YAxis yAxisId="right" orientation="right" stroke={`${theme === 'dark' ? '#eab308' : '#3b82f6'}`} width="auto" />
+                      <Tooltip />
+                      <Legend />
+                      <Bar yAxisId="left" dataKey="ทั้งหมด" fill={` ${theme === 'dark' ? '#3b82f6' : '#3b82f6'}`} />
+                      <Bar yAxisId="right" dataKey="เสร็จสิ้น" fill={` ${theme === 'dark' ? '#10b981' : '#10b981'}`} />
+                      <Bar yAxisId="right" dataKey="กำลังดำเนินการ" fill={` ${theme === 'dark' ? '#fb923c' : '#fb923c'}`} />
                     </BarChart>
                   </ResponsiveContainer>
                 </div>
